81b9c8422a586f6dc1496e2bace12118ccf30b97
Jeff Adams <jeffisadams@gmail.com>
2019-10-27 14:15:03 -0400

    Adding a switch to the index function to delete when a stack gets deleted.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as response from 'cfn-response';
-import { CloudFormationCustomResourceEventCommon, Callback, Context } from "aws-lambda";
+import { Callback, Context, CloudFormationCustomResourceEvent } from "aws-lambda";
 
 import { load_openapi_config, update_openapi_config } from './services/docs.service';
 import { S3Service } from './services/s3.service';
@@ -10,28 +10,38 @@ const SITE_BUCKET = process.env.SITE_BUCKET
  * Handles Uploading the S3 file, cleaning it and uploading it
  * This assumes happy path in many ways, but is a start
  */
-exports.handler = async (event: CloudFormationCustomResourceEventCommon, context: Context, callback: Callback) => {
+exports.handler = async (event: CloudFormationCustomResourceEvent, context: Context, callback: Callback) => {
   try {
     const key = event.ResourceProperties.DefinitionKey
     const config = await load_openapi_config()
-
-    const incomingSpec = JSON.parse(event.ResourceProperties.DefinitionBody)
-
-    // Add the key and upload the file
     const incomingSpecPath = `services/${key}.json`
-    S3Service.upload(SITE_BUCKET, incomingSpecPath, JSON.stringify(incomingSpec), 'application/json')
-    config.urls.push({
-      name: key,
-      url: incomingSpecPath
-    })
 
-    // Dedupe services in the config
-    const serviceHash = {}
-    config.urls.forEach(service => serviceHash[service.name] = service)
-    config.urls = Object.keys(serviceHash).map(s => serviceHash[s])
+    switch(event.RequestType) {
+      case 'Create':
+      case 'Update':
+        const incomingSpec = JSON.parse(event.ResourceProperties.DefinitionBody)
+
+        // Add the key and upload the file
+        S3Service.upload(SITE_BUCKET, incomingSpecPath, JSON.stringify(incomingSpec), 'application/json')
+        config.urls.push({
+          name: key,
+          url: incomingSpecPath
+        })
+    
+        // Dedupe services in the config
+        const serviceHash = {}
+        config.urls.forEach(service => serviceHash[service.name] = service)
+        config.urls = Object.keys(serviceHash).map(s => serviceHash[s])
+        break
+      case 'Delete':
+        await S3Service.delete(SITE_BUCKET, incomingSpecPath)
+        const updatedUrls = config.urls.filter(url => url.name !== key)
+        config.urls = updatedUrls
+        break
+    }
 
     await update_openapi_config(config)
-  
+
     // Upload the output results
     // await S3Service.upload(SITE_BUCKET, siteSpecPath, Buffer.from(JSON.stringify(api), 'utf-8'))
     response.send(event, context, response.SUCCESS, null)
